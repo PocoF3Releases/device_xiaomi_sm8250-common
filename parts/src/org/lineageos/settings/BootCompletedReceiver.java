3645309085d8201c19c6a339d3e4d8efdd518d65
johnmart19 <johnivan19999@gmail.com>
2024-04-16 06:15:28 +0300

    sm8250-common: Switch to DisplayFeature Color Profiles
    Following change is a complex change including overlays, DisplayFeature import, Sepolicy and Parts implementation
    
    Includes:
    * Introduce displayfeature color service and use MIUI color modes
    * Link displayfeature binder to death
    * Set standard display color mode in AOD
    * DisplayFeature Sepolicy
    * DisplayFeature VintF and hal & service
    * Allow parts to work with DisplayFeature
    * Import additional DisplayFeature props & alphabetize
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.settings.display.ColorService;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -33,5 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+        ColorService.startService(context);
     }
 }
