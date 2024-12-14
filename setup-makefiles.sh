df6baeae7ced22e011e9aae7fa2a5765fb085e07
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-14 02:03:34 +0100

    sm8250-common: setup-makefiles.sh: Fix tab indentation
    Change-Id: I3f26c15b039d013a186b4a0e54f5e66ffa9883c2

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,10 +25,10 @@ source "${HELPER}"
 function vendor_imports() {
     cat <<EOF >>"$1"
 		"device/xiaomi/sm8250-common",
-        "hardware/qcom-caf/common/libqti-perfd-client",
+		"hardware/qcom-caf/common/libqti-perfd-client",
 		"hardware/qcom-caf/sm8250",
 		"hardware/qcom-caf/wlan",
-        "hardware/xiaomi",
+		"hardware/xiaomi",
 		"vendor/qcom/opensource/commonsys/display",
 		"vendor/qcom/opensource/commonsys-intf/display",
 		"vendor/qcom/opensource/dataservices",
