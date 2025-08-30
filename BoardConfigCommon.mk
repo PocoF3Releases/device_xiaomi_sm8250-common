3c3e9b061a4c65c78c73c5d68881593348a5af0b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-30 23:45:15 +0800

    sm8250-common: Cleanup more flags
    These are gone after bp conversion for display and media
    
    Change-Id: I1169adf12d717417f5f328ebbcc01ecc9b911889

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,9 +115,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 KERNEL_CC := CC=clang
 override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
-# Media
-TARGET_USES_ION := true
-
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 ifeq ($(TARGET_IS_VAB),true)
