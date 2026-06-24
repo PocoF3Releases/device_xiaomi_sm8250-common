c4883290fa2c62cfa7946ad35a8cd86e29d96e8a
binbin323 <bin@dhao.cc>
2026-06-25 01:32:14 +0900

    sm8250-common: Patch wfdservice with libaudiobase

--- extract-files.py
+++ extract-files.py
@@ -41,6 +41,7 @@ blob_fixups: blob_fixups_user_type = {
      ): blob_fixup()
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libaudiobase.so')
