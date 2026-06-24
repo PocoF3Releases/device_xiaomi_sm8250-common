0423450dd147e5ccd18eb872130cec0086f5f1d4
binbin323 <bin@dhao.cc>
2026-06-24 20:21:40 +0900

    sm8250-common: Patch libwfdmmsrc_system with libgui_shim

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
 }  # fmt: skip
 
 
